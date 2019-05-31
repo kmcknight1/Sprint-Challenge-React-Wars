fc02725b940535525eec8311a67a5dfde7e5c2c7
kmcknight1 <katiemcknight1991@gmail.com>
2019-05-31 12:10:17 -0500

    more character info and added caseName function

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,12 +2,21 @@ import React from "react";
 import "./characters.css";
 
 const Character = props => {
-  console.log(props.character);
+
+    function caseWord(word) {
+        const [first, ...rest] = word
+        
+        return [first.toUpperCase(), ...rest].join('')
+      }
 
   return (
     <div className='character'>
     <h1>{props.character.name}</h1>
-    <h3>Gender: {props.character.gender === 'n/a' ? 'robot' : props.character.gender}</h3>
+    <h3>Gender: {props.character.gender === 'n/a' ? 'Robot' : caseWord(props.character.gender)}</h3>
+    <h3>Height: {props.character.height}cm</h3>
+    <h3>Birth Year: {props.character.birth_year}</h3>
+    <h3>Hair Color: {props.character.hair_color === 'n/a' ? 'N/A' : caseWord(props.character.hair_color)}</h3>
+    <h3>Eye Color: {props.character.eye_color === 'n/a' ? 'N/A' : caseWord(props.character.eye_color)}</h3>
     </div>
   );
 };
